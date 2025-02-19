69a2cee5370751dba5d36661bac5d54307ba7bb6
gimmoVatt <giammy.vatt@gmail.com>
2025-02-19 19:17:31 +0100

    blocco 1

---
+++ esercizi/es1-2.py
@@ -0,0 +1,13 @@
+
+
+def es_4_8():
+    numbers= list (1,11)
+
+    cubi=[] 
+    for n in numbers:
+        cubi.app9
+
+
+
+
+
---
+++ esercizi/es1.py
@@ -0,0 +1,39 @@
+
+
+
+'''l=[7,7,6,54,3,23,]
+l.remove()
+print(l)
+'''
+
+'''guest =["piero", "marco", "gio","fredrik"]
+
+guest.remove("piero")
+
+print(guest)
+
+guest.pop(1)
+
+print(guest)
+
+'''
+
+'''l =[1,5,6,7,89,76,56,43,56,"b"] 
+
+for x in range (len(l)-1,0,-1):
+    x=l(i)
+    print(x)
+
+'''
+'''
+
+
+
+'''
+ 
+
+
+    
+
+
+
