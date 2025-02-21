1a4b2e94290599b6878820574ffad3a459aa9c89
gimmoVatt <giammy.vatt@gmail.com>
2025-02-21 19:20:12 +0100

    ESERCIZI

--- esercizi/es1-2.py
+++ esercizi/es1-2.py
@@ -17,7 +17,7 @@ for k in dict:
     print ("poc nient")
 
 '''
-n=int(input("inserisci un numero:  "))
+''''n=int(input("inserisci un numero:  "))
 
 match n:
     case n if n >0:
@@ -33,9 +33,68 @@ match n:
 
 
 
+'''
+
+'''neonato=int(input("inserisci un numero di bambini : "))
+
+match neonato:
+    case 1:
+        print("congratulazioni")
+    case 2:
+        print("mortacci tua!!")
+    case 3:
+        print("ma stralimortacci tua!!")
+    case _:
+        print(f"ma stralimortacci tua {neonato} bambini !!")
 
 
 
+'''
+
+'''name=str(input("inserisci il propio nome :  "))
+genere=str(input("inserisci genere f o m:  "))
+print(f"name: {name}")
+
+match genere:
+    case "m":
+        print("è maschio")
+    case "f":
+        print("è femmina")
+    case _:
+        print(" è assessuato")
+        '''
+
+'''voto=int(input("inserisci un voto:  "))
+
+match voto:
+    case 10:
+        print("eccdellene")
+    case 8|9:
+       print("molkto buono")
+    case 6|7:
+        print("sufficente")
+    case 4|5:
+        print("non sufficente")
+    case _:
+        print("voto non valido")
+        '''
+
+
+oggetti=[]
+counter=0
+while counter<3:
+    oggetti.append(str(input("inserisci un oggetto: ")).lower)
+    counter+=1
+
+match oggetti:
+    case "penna","matita","quaderno":
+          print("scuola")
+    case "pane","latte","uova":
+          print("prodotti alimentari")
+    case "sedia","tavolo","armadio":
+          print("mobili")
+    case _:
+          print("non è nessuno di questi")
 
 
 
